85824923f006dba272cb9c2949e65acdc9647235
aravindan <aravindannr59@gmail.com>
2023-08-15 08:43:54 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:tablein/presentation/loginPage.dart';
 import 'package:tablein/presentation/signupPage.dart';
 import 'package:firebase_core/firebase_core.dart';
-Future main() async =>
-    WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializApp();
-runApp(TableIn());
-
+ Future main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp();
+   runApp(TableIn());
+ }
 class TableIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
